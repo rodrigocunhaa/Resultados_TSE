340aa71096fe33988cfc2a2a0c9be82c2bb25731
Rodrigo Cunha <rcalvesmoreira@gmail.com>
2020-12-19 21:45:02 -0300

    Fix favorite button locator

--- src/test/java/pages/CandidatePage.java
+++ src/test/java/pages/CandidatePage.java
@@ -4,7 +4,9 @@ import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.By;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import util.AppiumDriverManager;
 
@@ -19,15 +21,15 @@ public class CandidatePage {
         wait = AppiumDriverManager.getWait();
     }
 
+    @AndroidFindBy(uiAutomator = "new UiSelector().className(\"android.widget.Button\").textContains(\"Favoritar\")")
+    public MobileElement favoriteButton;
+
     @AndroidFindBy(uiAutomator = "new UiSelector().textContains(\"Fechar\")")
     public MobileElement closeButton;
 
     public void clickFavorButton() {
-        driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout" +
-                "/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout" +
-                "/android.view.ViewGroup/android.webkit.WebView/android.webkit.WebView/android.view.View" +
-                "/android.view.View/android.view.View/android.app.Dialog/android.view.View/android.view.View[2]" +
-                "/android.view.View[2]/android.view.View[5]/android.view.View[2]/android.widget.Button").click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("android.widget.Button")));
+        favoriteButton.click();
     }
 
     public void closePage() {
